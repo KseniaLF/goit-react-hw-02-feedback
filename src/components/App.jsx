4809b5e3de0369798d09727a3a9e507ecd5e4456
KseniaLF <llf9391@gmail.com>
2023-02-23 11:37:39 +0200

    delete imports

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,5 @@
-import Counter from './Counter';
-// import Counter from './Counter/Counter';
 import { Layout } from './Constants/Layout';
 import { GlobalStyle } from './Constants/GlobalStyle';
-import { Dropdown } from './DropDown/Dropdown';
 
 import { Feedback } from './Feedback/Feedback';
 
